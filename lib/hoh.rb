21d32b3f5af55597a5f7ab99315d626f82748a72
Doug Harrison <dougharrisong@gmail.com>
2020-01-10 19:57:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,6 +41,15 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-nestedHoh = {}
+nestedHoh = {
+  :PORTION_1 = {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+    sub_category: {
+    label: "Class"
+  }
+  
+}
 nestedHoh
 end

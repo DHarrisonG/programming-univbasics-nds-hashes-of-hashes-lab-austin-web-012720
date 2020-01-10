9059e2cfb0ed02dd7994ce90c29dfda8e73bc485
Doug Harrison <dougharrisong@gmail.com>
2020-01-10 20:03:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,16 +47,16 @@ nestedHoh = {
    sub_category: {label: "Phylum", sub_category: {label: "Class"}
   }
  },
-  :PORTION_2 = {
+  :PORTION_2 => {
    label: "Order"
  },
-  :PORTION_3 = {
+  :PORTION_3 => {
    label: "Family",
    sub_category: {
      label: "Genus",
    }
  },
-  :PORTION_4 = {
+  :PORTION_4 => {
    label: "Species",
    sub_category: nil
  }
